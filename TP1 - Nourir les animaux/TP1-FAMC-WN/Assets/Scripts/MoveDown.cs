0d4d6c88cfedaccc5e71b1056c95f0f36865f1c2
Eggduff <145608269+Eggduff@users.noreply.github.com>
2024-09-03 17:57:07 -0400

    Update MoveDown.cs

--- TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/MoveDown.cs
+++ TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/MoveDown.cs
@@ -8,11 +8,15 @@ public class MoveDown : MonoBehaviour
     public static PlayerController playerControllerScript;
 
     private float speed = 2f;
-    private float bottomBound = -15f;
+    private float bottomBound = -40f;
+    private Vector3 startPosition;
+    private float repeatWidth;
     // Start is called before the first frame update
     void Start()
     {
-
+        startPosition = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.z*4f;
+       // InvokeRepeating("SpawnGround", 0f, 5f);
     }
 
     // Update is called once per frame
@@ -20,11 +24,14 @@ public class MoveDown : MonoBehaviour
     {//Add if function to deal with game over. 
 
         transform.Translate(Vector3.back * speed * Time.deltaTime);
-
-
-        if(transform.position.z < bottomBound && gameObject.CompareTag("Ground")
+        if (transform.position.z < bottomBound && gameObject.CompareTag("Ground"))
         {
             Destroy(gameObject);
         }
+    }
+
+    void SpawnGround()
+    {
+
     }
 }
