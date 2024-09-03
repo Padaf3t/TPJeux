aca577d28df7bf2202956437def497ba2ce0be88
Eggduff <145608269+Eggduff@users.noreply.github.com>
2024-09-03 17:04:18 -0400

    Added ground. Added code to script MoveDown

--- TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/MoveDown.cs
+++ TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/MoveDown.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MoveDown : MonoBehaviour
 {
+
+    public static PlayerController playerControllerScript;
+
+    private float speed = 2f;
+    private float bottomBound = -15f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {//Add if function to deal with game over. 
+
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+
+        if(transform.position.z < bottomBound && gameObject.CompareTag("Ground")
+        {
+            Destroy(gameObject);
+        }
     }
 }
