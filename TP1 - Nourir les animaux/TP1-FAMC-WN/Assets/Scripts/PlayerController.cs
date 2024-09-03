71332fdedcbe0721550b6be556f9edda40ddb2e7
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-09-03 18:00:48 -0400

    trnavailler

--- TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/PlayerController.cs
+++ TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float horizontalInput;
+    private float lateralSpeed = 10f;
+    private float turnDegree;
+    private float maxDistanceFromZero = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if((transform.position.x > maxDistanceFromZero && horizontalInput > 0) ||
+                (transform.position.x < -maxDistanceFromZero && horizontalInput < 0))
+            {
+                horizontalInput = 0;
+            }
+
+        if(horizontalInput > 0)
+        {
+            //turnDe
+        }
+        else if(horizontalInput < 0)
+        {
+
+        }
+        transform.Translate(Vector3.right * lateralSpeed * horizontalInput * Time.deltaTime, Space.World);
+        transform.Rotate(
+            new Vector3 (0,Mathf.Clamp(transform.eulerAngles.y, -45, 45),0),
+             horizontalInput * turnDegree * Time.deltaTime);
         
+        
+        
+        
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            //Spawn bones
+        }
     }
+
+    
 }
