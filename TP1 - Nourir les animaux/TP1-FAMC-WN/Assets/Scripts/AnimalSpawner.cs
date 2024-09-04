8dbca29323f3767788f6ba7b0a8fd4a428ae03d0
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-09-04 18:22:33 -0400

    Update AnimalSpawner.cs

--- TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/AnimalSpawner.cs
+++ TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/AnimalSpawner.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class AnimalSpawner : MonoBehaviour
 {
+    public GameObject[] animals;
+
     private float normalDelay = 2f;
     private float nextDelay;
     private float progress = 0f;
     private Vector3 spawnPos;
+    private float zPos = 30f;
     private PlayerController playerController;
     private float xMax;
     private float xMin;
+    private float timeBetweenDifficulty = 15.0f;
+    private float difficultyProgress = 0.0f;
+    
         
     // Start is called before the first frame update
     void Start()
     {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         xMax = playerController.GetMaxDistanceFromZero();
         xMin = -xMax;
     }
@@ -23,18 +30,28 @@ public class AnimalSpawner : MonoBehaviour
     void Update()
     {
         progress += Time.deltaTime;
+        difficultyProgress += Time.deltaTime;
 
         if (progress >= nextDelay)
         {
             progress = 0f;
             SpawnObstacle();
 
-            nextDelay = Random.Range(0.85f * normalDelay, 1.5f * normalDelay);
+            nextDelay = Random.Range(0.50f * normalDelay, 1.5f * normalDelay);
+        }
+
+        if(difficultyProgress >= timeBetweenDifficulty)
+        {
+            difficultyProgress = 0f;
+            normalDelay *= 0.95f;
         }
     }
 
     private void SpawnObstacle()
     {
-        
+        var xPos = Random.Range(xMin, xMax);
+        spawnPos = new Vector3(xPos, 0, zPos);
+        Instantiate(animals[0], spawnPos, animals[0].transform.rotation);
+
     }
 }
