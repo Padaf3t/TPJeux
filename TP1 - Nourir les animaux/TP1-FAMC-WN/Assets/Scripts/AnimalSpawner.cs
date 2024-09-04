5a8f98cea2530d2bc879a5980ff9eecfd6425431
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-09-04 17:42:03 -0400

    add

--- TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/AnimalSpawner.cs
+++ TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/AnimalSpawner.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class AnimalSpawner : MonoBehaviour
 {
+    private float normalDelay = 2f;
+    private float nextDelay;
+    private float progress = 0f;
+    private Vector3 spawnPos;
+    private PlayerController playerController;
+    private float xMax;
+    private float xMin;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+        xMax = playerController.GetMaxDistanceFromZero();
+        xMin = -xMax;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        progress += Time.deltaTime;
+
+        if (progress >= nextDelay)
+        {
+            progress = 0f;
+            SpawnObstacle();
+
+            nextDelay = Random.Range(0.85f * repeatDelay, 1.5f * repeatDelay);
+        }
+    }
+
+    private void SpawnObstacle()
     {
         
     }
