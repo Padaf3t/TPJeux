e5b4039923e9c070c40cc7f58ea193d6ceb75238
FelixCarrier <129521661+Padaf3t@users.noreply.github.com>
2024-09-08 08:07:03 -0400

    Clean PlayerController

--- TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/PlayerController.cs
+++ TP1 - Nourir les animaux/TP1-FAMC-WN/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     private GameOverTrigger gameOverTrigger;
+    //movement variable
     private float horizontalInput;
     private float lateralSpeed = 10f;
+    private float maxDistanceFromZero = 10f;
+    //rotation
     private float currentRotation;
     private float rotationSpeed = 150;
     private float rotationDirection;
-    private float maxDistanceFromZero = 10f;
+    //Projectile variable
     private float distanceFromPlayer = 0.7f;
-    public GameObject projectileObject;
+    [SerializeReference]private GameObject projectileObject;
+    //Animation
     private Animator playerAnim;
 
     // Start is called before the first frame update
@@ -33,20 +38,7 @@ public class PlayerController : MonoBehaviour
 
             SetMovement();
 
-            if (horizontalInput == 0)
-            {
-                if (currentRotation < 0)
-                {
-                    rotationDirection = 1;
-                }
-                else if (currentRotation > 0)
-                {
-                    rotationDirection = -1;
-                }
-            }
-
-            var newRotation = currentRotation + rotationDirection * rotationSpeed * Time.deltaTime;
-            SetCurrentRotation(newRotation);
+            SetRotation();
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -61,12 +53,6 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void SetCurrentRotation(float rot)
-    {
-        currentRotation = Mathf.Clamp(rot, -45, 45);
-        transform.rotation = Quaternion.Euler(0, rot, 0);
-    }
-
     private void SetMovement()
     {
         if ((transform.position.x > maxDistanceFromZero && horizontalInput > 0) ||
@@ -75,6 +61,26 @@ public class PlayerController : MonoBehaviour
             horizontalInput = 0;
         }
         transform.Translate(Vector3.right * lateralSpeed * horizontalInput * Time.deltaTime, Space.World);
+    }
+
+    private void SetRotation()
+    {
+        if (horizontalInput == 0)
+        {
+            if (currentRotation < 0)
+            {
+                rotationDirection = 1;
+            }
+            else if (currentRotation > 0)
+            {
+                rotationDirection = -1;
+            }
+        }
+
+        var newRotation = currentRotation + rotationDirection * rotationSpeed * Time.deltaTime;
+
+        currentRotation = Mathf.Clamp(newRotation, -45, 45);
+        transform.rotation = Quaternion.Euler(0, newRotation, 0);
     }
 
     public float GetMaxDistanceFromZero() { return maxDistanceFromZero; }
