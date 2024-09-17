4d55e689ddc4383873fd4e60256008a8dc71aec8
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-09-17 16:48:21 -0400

    Implementation force joueur

--- TP2/TP2-SumoBall/Assets/Script/PlayerController.cs
+++ TP2/TP2-SumoBall/Assets/Script/PlayerController.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float verticalInput;
+    private float speed = 300f;
+    private Rigidbody rb;
+    private Camera cam;
+    public static GameObject playerObject;
+
+    //Timer
+    private float progress = 0f;
+    private float timerActualPowerUp;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main;
+        playerObject = this.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(cam.transform.forward * speed * verticalInput, ForceMode.Force);
+
+    }
+
+    //Reste a ajouter le powerUpType dans les paramètres
+    private void EnablePowerUp()
+    {
+        //À implémenter
     }
 }
