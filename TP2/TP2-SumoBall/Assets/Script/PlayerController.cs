713b0e70183d3b3166d260c2a87a50992aa7dbd5
Eggduff <145608269+Eggduff@users.noreply.github.com>
2024-09-24 18:14:46 -0400

    work on powerUp2

--- TP2/TP2-SumoBall/Assets/Script/PlayerController.cs
+++ TP2/TP2-SumoBall/Assets/Script/PlayerController.cs
@@ -12,10 +12,20 @@ public class PlayerController : MonoBehaviour
     private Camera cam;
     public static GameObject playerObject;
 
+    //powerUp
+    private bool powerUp1IsActive;
+    private bool powerUp2IsActive = true;
+    private float powerUp1Duration;
+    private float powerUpStrengthWeight = 15f;
+    private int powerUp2Uses = 3;
+    private float intangibleTimer = 2f;
+
+
+
     //Timer
     private float progress = 0f;
     private float timerActualPowerUp;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +38,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
+        UpdatePowerUp1();
+        UpdatePowerUp2();
+
     }
 
     private void FixedUpdate()
@@ -37,13 +50,94 @@ public class PlayerController : MonoBehaviour
         dir.Normalize();
         rb.AddForce(dir * speed * verticalInput, ForceMode.Force);
 
+
+
+    }
+
+    private void UpdatePowerUp1()
+    {
+        if (powerUp1IsActive)
+        {
+
+            if (powerUp1Duration > 0)
+            {
+                powerUp1Duration -= Time.deltaTime;
+            }
+            else
+            {
+                powerUp1Duration = 0;
+                powerUp1IsActive = false;
+                this.rb.mass -= powerUpStrengthWeight;
+
+            }
+        }
+    }
+
+
+    //todo
+    private void UpdatePowerUp2()
+    {
+        if (powerUp2IsActive)
+        {
+            intangibleTimer -= Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space) && powerUp2Uses > 0)
+            {
+                if (intangibleTimer > 0)
+                {
+                    rb.isKinematic = false;
+                    powerUp2Uses -= 1;
+                }
+                else
+                {
+                    rb.isKinematic = true;
+                    intangibleTimer = 0;
+                }
+            }
+            else if (powerUp2Uses == 0)
+            {
+                powerUp2IsActive = false;
+            }
+
+        }
     }
 
     //Reste a ajouter le powerUpType dans les paramètres
     public void EnablePowerUp(PowerUp.PowerUpType type)
     {
-        if(type == PowerUp.PowerUpType.POWERUP01) { }
+        if (type == PowerUp.PowerUpType.POWERUP01)
+        {
+            powerUp1();
+        }
+        else if(type == PowerUp.PowerUpType.POWERUP02)
+        {
+            powerUp2();
+        }
 
 
     }
+
+    private void powerUp1()
+    {
+        powerUp1Duration += 15;
+        if (!powerUp1IsActive)
+        {
+            powerUp1IsActive = true;
+            this.rb.mass += powerUpStrengthWeight;
+        }
+    }
+
+    private void EndPowerUp1()
+    {
+        
+    }
+
+    private void powerUp2()
+    {
+        powerUp2Uses = 3;
+        powerUp2IsActive = true;
+    }
+
+
+    
+
 }
