264a0e72b64829e73e614d2c2767bc7ce002700b
William Nolan <145608269+Eggduff@users.noreply.github.com>
2024-09-24 13:43:05 -0400

    enemy controller

--- TP2/TP2-SumoBall/Assets/Script/EnemyController.cs
+++ TP2/TP2-SumoBall/Assets/Script/EnemyController.cs
@@ -10,7 +10,7 @@ public class EnemyController : MonoBehaviour
     private float pullForce = -10f;
     Rigidbody rb;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,11 +26,32 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void InitializeEnemy()
+    private GameObject InitializeEnemy(int level)
     {
+        if (level == 1)
+        {
+            rb.mass = 1.0f;
+        }
+        else if (level == 2)
+        {
+            rb.mass = 10.0f;
+        }
+        else if (level == 3)
+        {
+            rb.mass = 20.0f;
+        }
+        else if (level == 4)
+        {
+            rb.mass = 50.0f;
+        }
+        else 
+        {
+            rb.mass = 100.0f;
+        }
 
+        return this.gameObject;
     }
 }
